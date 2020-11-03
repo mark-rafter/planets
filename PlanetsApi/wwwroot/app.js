e7f3c6280881b0e1543f5ac612a102d9211ff2c0
mark <38855689+mark-rafter@users.noreply.github.com>
2020-11-03 09:29:44 +0000

    basic planet-info component

--- PlanetsApi/wwwroot/app.js
+++ PlanetsApi/wwwroot/app.js
@@ -1,14 +1,36 @@
-const App = {
+const Index = {
     data() {
         return {
-            selectedPlanet: null,
+            planetInfo: null,
             planetNames: []
         }
     },
+    methods: {
+        async updatePlanetInfo(planetName) {
+            const planetInfoResponse = await fetch(`/planet/${planetName}`);
+            this.planetInfo = planetInfoResponse.ok ? await planetInfoResponse.json() : null;
+        }
+    },
     async created() {
-        const planetsResponse = await fetch("/planet");
+        const planetsResponse = await fetch('/planet');
         this.planetNames = await planetsResponse.json();
     }
 };
 
-Vue.createApp(App).mount('#app');
+const app = Vue.createApp(Index);
+
+app.component('planet-info', {
+    props: {
+        planetInfo: Object
+    },
+    template: `
+        <h3>{{ planetInfo.name }}</h3>
+        <img src="{{ planetInfo.imageUrl }}" />
+        <ul>
+            <li>Distance from the sun: {{ planetInfo.distance }}</li>
+            <li>Mass: {{ planetInfo.mass }}</li>
+            <li>Diameter: {{ planetInfo.diameter }}</li>
+        </ul>`
+});
+
+app.mount('#app');
